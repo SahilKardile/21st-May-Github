b31df68f8250e2499e1f122ec0b556c38b18191f
Sahil <kardilesahil@gmail.com>
2022-11-20 11:06:59 +0530

    This is first commit

---
+++ src/Sample/Test1.java
@@ -0,0 +1,12 @@
+package Sample;
+
+public class Test1 
+{
+   public static void main(String[]args)
+   {
+	  
+	   System.out.println("Good morning its 20th Nov and Sunday today");
+	   
+	   
+   }
+}
